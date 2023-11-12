a6de03a05f8b280653629d0e43f0bd125efe4d00
Andreu Orenga <andreuorenga@gmail.com>
2023-11-12 21:08:33 +0100

    se terminan ejercicios numeros aleatorios y fibonacci

--- JAVA/EJERCICIOS ARRAYS/src/ejemploArray2.java
+++ JAVA/EJERCICIOS ARRAYS/src/ejemploArray2.java
@@ -2,19 +2,41 @@ import java.util.Scanner;
 
 public class ejemploArray2 {
     public static void main(String[] args) {
-        int[] array = new int[7];
+
+        int[] array = new int[10];
+        int total = 0;
+
         Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el numero que quieres encontrar " + "(entre el 0 y el 20): ");
 
-        System.out.println("introduce el numero por consola: ");
-        System.out.println("Tu array tiene los siguentes valores:");
+        int numero = teclado.nextInt();
 
+
+        //llenamos el array
         for (int indice = 0; indice < array.length; indice++) {
-           array[indice] = (int) (Math.random() * 10 + 1);
+           array[indice] = (int) (Math.random() * 20 + 1);
         }
 
+        //comprobacion
         for (int indice = 0; indice < array.length; indice++) {
-            System.out.print("[" + array[indice] + "]");
-        }
+            if(array[indice]==numero) {
+                total++;
+            }
 
+        }
+        System.out.print("El array tiene los siguientes valores: ");
+
+        for (int indice =0; indice < array.length; indice++){
+            System.out.println("[" + array[indice]+"]");
+
+        }
+        if (total == 0){  //\n fuerza el salto de linea
+
+            System.out.println("\nEl numero que buscabas no se ha encontrado.");
+
+        } else {
+
+            System.out.println("\nEl numero que buscabas se encontrado " + total +" veces");
+        }
     }
 }
